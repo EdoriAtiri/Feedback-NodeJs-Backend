f01c881a01bfe1076f2f415a62f3291a6c9ca01c
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-02-21 12:33:22 +0100

    Add procfile and updated ports

--- app.js
+++ app.js
@@ -99,6 +99,11 @@ app.use('/feedbacks', feedbackRouter)
 //     })
 //   })
 
-app.listen(5000, function () {
-  console.log('Server started on port 5000')
+let port = process.env.PORT
+if (port == null || port == '') {
+  port = 5000
+}
+
+app.listen(port, function () {
+  console.log('Server Started Successfully.')
 })
